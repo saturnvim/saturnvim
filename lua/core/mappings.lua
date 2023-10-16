14e8e7afb72be763ad64b94bd8a987e35ad5f964
vclemenzi <vclemenzi@users.noreply.github.com>
2023-10-16 19:04:27 +0200

    feat: split shortcut

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -6,3 +6,6 @@ vim.api.nvim_set_keymap("n", "<C-k>", [[ <C-w>k ]], { noremap = true, silent = t
 vim.api.nvim_set_keymap("n", "[", [[<cmd>bprevious<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "]", [[<cmd>bnext<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>bd", [[<cmd>bd<CR>]], { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<C-s>", [[<cmd>split<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-v>", [[<cmd>vsplit<CR>]], { noremap = true, silent = true })
