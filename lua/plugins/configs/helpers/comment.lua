6c7aa1c5acb2ca7028aa2e2b478fd3c950c01883
vclemenzi <vclemenzi@users.noreply.github.com>
2023-10-18 15:40:34 +0200

    feat: lazy load some plugins

--- lua/plugins/configs/helpers/comment.lua
+++ lua/plugins/configs/helpers/comment.lua
@@ -1,14 +1,15 @@
 return {
-    "numToStr/Comment.nvim",
-    keys = {
-      { "gcc", mode = "n", desc = "Comment current line" },
-      { "gc", mode = { "n", "o" }, desc = "Comment linewise" },
-      { "gc", mode = "x", desc = "Comment linewise" },
-      { "gbc", mode = "n", desc = "Comment current block" },
-      { "gb", mode = { "n", "o" }, desc = "Comment blockwise" },
-      { "gb", mode = "x", desc = "Comment blockwise" },
-    },
-    config = function(_, opts)
-      require("Comment").setup(opts)
-    end,
+  "numToStr/Comment.nvim",
+  lazy = true,
+  keys = {
+    { "gcc", mode = "n",          desc = "Comment current line" },
+    { "gc",  mode = { "n", "o" }, desc = "Comment linewise" },
+    { "gc",  mode = "x",          desc = "Comment linewise" },
+    { "gbc", mode = "n",          desc = "Comment current block" },
+    { "gb",  mode = { "n", "o" }, desc = "Comment blockwise" },
+    { "gb",  mode = "x",          desc = "Comment blockwise" },
+  },
+  config = function(_, opts)
+    require("Comment").setup(opts)
+  end,
 }
